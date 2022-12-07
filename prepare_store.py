36ecc9ff7c9fc83048d5d529fe5f40ae531eee07
racheldownen <rae.l.downen@gmail.com>
2022-12-06 19:49:11 -0600

    update

--- prepare_store.py
+++ prepare_store.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 from datetime import timedelta, datetime
 import numpy as np
@@ -15,39 +16,22 @@ plt.rc('figure', figsize=(13, 7))
 plt.style.use('seaborn-whitegrid')
 plt.rc('font', size=16)
 
-
-# convert the sale_date from object to date time
-def convert_datetime():
-    df.sale_date = pd.to_datetime(df.sale_date)
-
-
-# plot the sale amount by date
-def by_date():
-    by_date = df.groupby(['sale_date']).sale_amount.sum().reset_index()
-    by_date.plot(x='sale_date', y='sale_amount')
-
-    
-# set index by datetime variable
-def set_index():
-    df = df.set_index("sale_date").sort_index()
-
-
 # add month and day of week to df columns
-def dt_variable():
+def add_dt_column():
     df['month'] = df.index.month
     df['day_of_week'] = df.index.day_name
+    return df
 
-    
 # add total sales column - multiply the price by amount
-def tot_sales():
+def total_sales():
     df['sales_total'] =  df['sale_amount'] * df['item_price']
-    
+    return df
 ################################################
     
 def prep_store_data(df):
     # Reassign the sale_date column to be a datetime type
     df.sale_date = pd.to_datetime(df.sale_date)
-
+    
     # Sort rows by the date and then set the index as that date
     df = df.set_index("sale_date").sort_index()
     return df
